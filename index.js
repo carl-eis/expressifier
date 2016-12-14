b17b0dfdd1416dccdd3d55cfc957d808f7ff320e
Carl Eiserman <carleiserman@gmail.com>
2016-12-14 20:53:40 +0200

    Abalobi: Added java job & request data

--- index.js
+++ index.js
@@ -10,13 +10,53 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 
+var spawn = require("child_process").spawn,
+    child;
+
+
 app.use(cors());
+var responseJSON = {};
 
 app.get('/', function(req, res) {
-  res.headers = {
-    "Access-Control-Allow-Origin": "*"
-  }
-    res.send('Hello World!')
+    res.headers = {
+        "Access-Control-Allow-Origin": "*"
+    };
+
+    //Build Response
+
+    responseJSON.data = "Hello World!";
+    responseJSON.test = {
+        test1: "test1 text",
+        test2: "test2 text"
+    }
+
+    //Response data
+    console.log("Firing JAVA job...");
+    logResponse("Firing JAVA job...");
+
+    child = spawn("powershell.exe", ["C:\\cygwin64\\home\\Carl\\Techairos\\odk2-publisher\\target\\bin\\testpost.bat"]);
+    child.stdout.on("data", function(data) {
+        console.log("Powershell Data: " + data);
+        logResponse("Powershell Data: " + data);
+    });
+    child.stderr.on("data", function(data) {
+        console.log("Powershell Errors: " + data);
+        logResponse("Powershell Errors: " + data);
+    });
+    child.on("exit", function() {
+        console.log("Powershell Script finished");
+        logResponse("Powershell Script finished");
+        res.send(responseJSON);
+        console.log(JSON.stringify(res.headers, null, 4));
+
+    });
+    child.stdin.end(); //end input
+
+
+
+
+    // res.send(responseJSON);
+    // console.log(JSON.stringify(res.headers, null, 4));
 });
 
 app.post('/', function(req, res) {
@@ -31,7 +71,7 @@ app.delete('/user', function(req, res) {
     res.send('Got a DELETE request at /user')
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
     console.log('Example app listening on port 3000!')
 });
 
@@ -40,3 +80,6 @@ app.listen(3000, function () {
 //   res.header("Access-Control-Allow-Headers", "X-Requested-With");
 //   next();
 // });
+function logResponse(data) {
+    responseJSON.data += "\n" + data.toString();
+}
